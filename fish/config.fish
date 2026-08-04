ddfd76660c3913b829ff6e4adae95af2a4a0268f
Rodrigo Morais <rmoraisu@protonmain.com>
2026-08-04 10:37:45 -0300

    add initial config files

---
+++ fish/config.fish
@@ -0,0 +1,16 @@
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# overwrite greeting
+# potentially disabling fastfetch
+#function fish_greeting
+#    # smth smth
+#end
+
+# SSH Agent
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+    ssh-add ~/.ssh/github 2>/dev/null
+end
+
+# Pi
+fish_add_path "$HOME/.local/bin"
